8803f28e58bba4b5ef36fb434e507a2063a38184
Sky <79532409+Bormey-Sky@users.noreply.github.com>
2024-12-26 09:31:04 +0700

    changes registered api, input box

--- server.py
+++ server.py
@@ -86,7 +86,7 @@ async def register(song: UploadFile, song_title: str = Body()):
         [
             {
                 "embedding": embedding[0],
-                "song_title": song.filename,
+                "song_title": song_title,
                 "start_position_sec": 0,
                 "end_position_sec": int(math.ceil(audio.shape[0] / 32000)),
                 "song_id": 0,
